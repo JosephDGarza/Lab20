fc7fbabf04a44202c47451507278efd3835abcd7
JosephDGarza <32659326+JosephDGarza@users.noreply.github.com>
2017-11-28 18:23:51 -0500

    inputs go to myqls db

--- src/com/as/samples/pageOneProcess.java
+++ src/com/as/samples/pageOneProcess.java
@@ -2,11 +2,17 @@ package com.as.samples;
 //FOLLOWED THE YOUTUBE VIDEO BELOW
 //https://www.youtube.com/watch?v=kR-gaI0utQA
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Statement;
 
 /**
  * Servlet implementation class pageOneProcess
@@ -25,6 +31,8 @@ public class pageOneProcess extends HttpServlet {
 	String email=request.getParameter("email");
 	String phoneNumber=request.getParameter("phoneNumber");
 	String password=request.getParameter("password");
+	
+	
 	//CONSOLE LOGS 
 	System.out.println("Thank you for registering:" +firstName + " " + lastName);
 	System.out.println("confirming your details:");
@@ -39,7 +47,24 @@ public class pageOneProcess extends HttpServlet {
 	request.getSession().setAttribute("phoneNumber", phoneNumber);
 	request.getSession().setAttribute("password", password);
 	
+	
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/coffeeshopdb","gcjdbc","gcjdbc");
+		Statement stmt = (Statement) con.createStatement();
+		stmt.execute("insert into users value('"+firstName+"','"+lastName+"','"+email+"','"+phoneNumber+"','"+password+"')");
+		System.out.println("data inserted" + phoneNumber);
+		
+		
+		
+	} catch (ClassNotFoundException | SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
 	response.sendRedirect("html/pageTwo.html");
+	
 	}
 
 }
